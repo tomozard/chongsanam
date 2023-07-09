787ca69ce41c68ca39f2b74c44ffa14094fece3b
arsan_t <arsan_t@apthai.com>
2023-07-09 22:37:52 +0700

    add first code

---
+++ pages/_app.tsx
@@ -0,0 +1,145 @@
+import { GitHubBanner, Refine } from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import {
+  notificationProvider,
+  RefineThemes,
+  ThemedLayoutV2,
+  ThemedTitleV2,
+} from "@refinedev/mantine";
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/nextjs-router";
+import type { NextPage } from "next";
+import { AppProps } from "next/app";
+
+import { Header } from "@components/header";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  Global,
+  MantineProvider,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
+import { NotificationsProvider } from "@mantine/notifications";
+import dataProvider, { GraphQLClient } from "@refinedev/hasura";
+import { appWithTranslation, useTranslation } from "next-i18next";
+import { authProvider } from "src/authProvider";
+import { AppIcon } from "src/components/app-icon";
+
+const API_URL = "https://flowing-mammal-24.hasura.app/v1/graphql";
+
+const client = new GraphQLClient(API_URL, {
+  headers: {
+    "x-hasura-role": "public",
+  },
+});
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  noLayout?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
+  const renderComponent = () => {
+    if (Component.noLayout) {
+      return <Component {...pageProps} />;
+    }
+
+    return (
+      <ThemedLayoutV2
+        Header={() => <Header sticky />}
+        Title={({ collapsed }) => (
+          <ThemedTitleV2
+            collapsed={collapsed}
+            text="Chong Sanam"
+            icon={<AppIcon />}
+          />
+        )}
+      >
+        <Component {...pageProps} />
+      </ThemedLayoutV2>
+    );
+  };
+
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true,
+  });
+  const { t, i18n } = useTranslation();
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorSchemeProvider
+          colorScheme={colorScheme}
+          toggleColorScheme={toggleColorScheme}
+        >
+          {/* You can change the theme colors here. example: theme={{ ...RefineThemes.Magenta, colorScheme:colorScheme }} */}
+          <MantineProvider
+            theme={{ ...RefineThemes.Purple, colorScheme: colorScheme }}
+            withNormalizeCSS
+            withGlobalStyles
+          >
+            <Global styles={{ body: { WebkitFontSmoothing: "auto" } }} />
+            <NotificationsProvider position="top-right">
+              <Refine
+                routerProvider={routerProvider}
+                dataProvider={dataProvider(client)}
+                notificationProvider={notificationProvider}
+                authProvider={authProvider}
+                i18nProvider={i18nProvider}
+                resources={[
+                  {
+                    name: "blog_posts",
+                    list: "/blog-posts",
+                    create: "/blog-posts/create",
+                    edit: "/blog-posts/edit/:id",
+                    show: "/blog-posts/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
+                  },
+                  {
+                    name: "categories",
+                    list: "/categories",
+                    create: "/categories/create",
+                    edit: "/categories/edit/:id",
+                    show: "/categories/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
+                  },
+                ]}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                }}
+              >
+                {renderComponent()}
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+            </NotificationsProvider>
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </RefineKbarProvider>
+    </>
+  );
+}
+
+export default appWithTranslation(MyApp);
---
+++ pages/blog-posts/edit/[id].tsx
@@ -0,0 +1,36 @@
+import { MantineEditInferencer } from "@refinedev/inferencer/mantine";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { authProvider } from "src/authProvider";
+
+import { inferencerPredefinedMeta } from "src/inferencerPredefinedMeta";
+
+export default function BlogPostEdit() {
+  return <MantineEditInferencer meta={inferencerPredefinedMeta} />;
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (!authenticated) {
+    return {
+      props: {
+        ...translateProps,
+      },
+      redirect: {
+        destination: `${redirectTo}?to=${encodeURIComponent("/blog-posts")}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
