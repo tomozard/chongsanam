7da722da91bda4e0b2b608850dbc5744a1ee0ebe
arsan_t <arsan_t@apthai.com>
2023-07-10 00:22:22 +0700

    comment normalizefile

--- src/utility/normalize.ts
+++ src/utility/normalize.ts
@@ -1,29 +1,29 @@
-import { UploadFile } from "antd/lib/upload/interface";
+// import { UploadFile } from "antd/lib/upload/interface";
 
-interface UploadResponse {
-    url: string;
-    fileId: string;
-}
-interface EventArgs<T = UploadResponse> {
-    file: UploadFile<T>;
-    fileList: Array<UploadFile<T>>;
-}
+// interface UploadResponse {
+//     url: string;
+//     fileId: string;
+// }
+// interface EventArgs<T = UploadResponse> {
+//     file: UploadFile<T>;
+//     fileList: Array<UploadFile<T>>;
+// }
 
-export const normalizeFile = (event: EventArgs) => {
-    const { fileList } = event;
+// export const normalizeFile = (event: EventArgs) => {
+//     const { fileList } = event;
 
-    return fileList.map((item) => {
-        const { uid, name, type, size, response, percent, status } = item;
+//     return fileList.map((item) => {
+//         const { uid, name, type, size, response, percent, status } = item;
 
-        return {
-            uid,
-            name,
-            url: item.url || response?.url,
-            type,
-            size,
-            percent,
-            status,
-            fileId: item.response?.fileId || response?.fileId,
-        };
-    });
-};
+//         return {
+//             uid,
+//             name,
+//             url: item.url || response?.url,
+//             type,
+//             size,
+//             percent,
+//             status,
+//             fileId: item.response?.fileId || response?.fileId,
+//         };
+//     });
+// };
